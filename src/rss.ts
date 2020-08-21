f19b0aade6bf443e7f6e45f93b95d7d4eb4f4fd8
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-08-21 11:45:04 +0700

    Alpha release 0.0.2 (#1)

---
+++ src/rss.ts
@@ -0,0 +1,61 @@
+const chars: { [key: string]: string } = {
+	'"': 'quot',
+	"'": '#39',
+	'&': 'amp',
+	'<': 'lt',
+	'>': 'gt',
+};
+
+const formatPubDate = (date: string) => {
+	const formatted = new Date(date);
+	return formatted.toUTCString();
+};
+const clean = (html: string) => {
+	if (!html) return '';
+	return html.replace(/["'&<>]/g, (c) => `&${chars[c]};`);
+};
+
+export default function RSS(channel: RSSChannel, items: RSSItem[]) {
+	const createItem = (item: RSSItem) => `
+		<item>
+			<title>${clean(item.title)}</title>
+			<link>https://${channel.domain}/${clean(item.slug)}</link>
+			<description>${clean(item.description)}</description>
+			<pubDate>${formatPubDate(item.date)}</pubDate>
+		</item>`;
+
+	const xml = `
+	<?xml version="1.0" encoding="UTF-8" ?>
+	<rss version="2.0">
+	<channel>
+		<title>${clean(channel.title)}</title>
+		<link>https://${channel.domain}/${clean(channel.slug)}</link>
+		<description>${clean(channel.description)}</description>
+		<lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+		<language>${channel.language || 'en'}</language>
+		<image>
+			<url>https://${channel.domain}/${channel.image || 'favicon.ico'}</url>
+			<title>${channel.title}</title>
+			<link>https://${channel.domain}/${clean(channel.slug)}</link>
+		</image>
+		${items.map(createItem).join('')}
+	</channel>
+	</rss>`;
+
+	return xml.replace(/>[^\S]+/gm, '>').trim();
+}
+
+interface RSSChannel {
+	domain: string;
+	image?: string;
+	title: string;
+	slug: string;
+	description: string;
+	language?: string;
+}
+interface RSSItem {
+	title: string;
+	slug: string;
+	description: string;
+	date: string;
+}
---
+++ svelte/components/Edit.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let repo, path;
+	import { Link, Icon } from '@ignatiusmb/elements';
+	const link = `https://github.com/${repo}/edit/master`;
+</script>
+
+<span>
+	<Link href="{link}/{path}">
+		<Icon name="edit" />
+	</Link>
+</span>
+
+<style>
+	span {
+		display: flex;
+		align-items: center;
+		color: var(--aqua-primary);
+	}
+	span :global(a) {
+		display: flex;
+	}
+</style>
